eb8c83450b92c516300ef7f7cc02360ef9a4f303
Hosein Darabi <innovator.h.darabi@gmail.com>
2026-03-20 05:43:56 +0100

    Update profile photo with cropped version (3.jpg)

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -120,7 +120,7 @@ export default function Hero() {
 
             <div className="relative overflow-hidden rounded-2xl border border-accent/10 bg-eerie-black-1">
               <Image
-                src="/images/3.png"
+                src="/images/3.jpg"
                 alt="Hossein Darabi"
                 width={480}
                 height={600}
